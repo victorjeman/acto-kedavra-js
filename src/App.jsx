b457f9b798bf42f83b55c5ea75ee668c474c5421
Victor JEMAN <victorjeman@gmail.com>
2022-06-20 21:45:56 +0300

    lazy commit name

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
+import { Hobby } from 'components/Hobby/Hobby'
+import { Hobbies } from 'components/Hobbies/Hobbies'
+import { ReadMore } from 'components/ReadMore/ReadMore'
+import { ActorReadMore } from 'components/ActorReadMore/ActorReadMore'
+import { Actor } from 'components/Actor/Actor'
 import { Actors } from 'components/Actors/Actors'
 
-const MY_ACTOR = {
-  image: 'https://victorjeman.com/_next/static/images/me1-d09094b6bccc9c6c4291d8ab34d48b2f.jpg',
-  name: 'Victor Jeman',
-  occupation: 'Doing things',
-  likes: 30,
-  hobbies: ['coding', 'gym', 'reading'],
-  description:
-    'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam iste excepturi harum reiciendis natus repellendus nostrum at amet, nobis id sint minima voluptas omnis dicta illo architecto! Ipsa, minima molestias!',
-}
-
-const ACTORS = [MY_ACTOR, MY_ACTOR, MY_ACTOR, MY_ACTOR]
+import { MY_ACTOR, ACTORS } from 'data'
 
 function App() {
   return (
     <div className='app'>
-      {/* <Hobbies hobbies={['Hobby 1', 'Hobby 2', 'Hobby 3']} />
-      <ReadMore text='Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam iste excepturi harum reiciendis natus repellendus nostrum at amet, nobis id sint minima voluptas omnis dicta illo architecto! Ipsa, minima molestias!' /> */}
+      {/* <Hobby name='Coding' /> */}
+      {/* <Hobbies hobbies={['Hobby 1', 'Hobby 2', 'Hobby 3', 'Hobby 4']} /> */}
+
+      {/* <ReadMore text='Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam iste excepturi harum reiciendis natus repellendus nostrum at amet, nobis id sint minima voluptas omnis dicta illo architecto! Ipsa, minima molestias!' /> */}
+
+      {/* <ActorReadMore text='Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam iste excepturi harum reiciendis natus repellendus nostrum at amet, nobis id sint minima voluptas omnis dicta illo architecto! Ipsa, minima molestias!' /> */}
 
       {/* <Actor {...MY_ACTOR} /> */}
       <Actors actors={ACTORS} />
---
+++ src/data.js
@@ -0,0 +1,11 @@
+export const MY_ACTOR = {
+  image: 'https://victorjeman.com/_next/static/images/me1-d09094b6bccc9c6c4291d8ab34d48b2f.jpg',
+  name: 'Victor Jeman',
+  occupation: 'Doing things',
+  likes: 30,
+  hobbies: ['coding', 'gym', 'reading', 'doing nothing'],
+  description:
+    'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam iste excepturi harum reiciendis natus repellendus nostrum at amet, nobis id sint minima voluptas omnis dicta illo architecto! Ipsa, minima molestias!',
+}
+
+export const ACTORS = [MY_ACTOR, MY_ACTOR, MY_ACTOR, MY_ACTOR]
