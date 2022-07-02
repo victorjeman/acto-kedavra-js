a0f4863ca80d68034d89ce5be71bf2822fe37cd4
Victor Jeman <victorjeman@users.noreply.github.com>
2022-07-02 23:50:52 +0300

    Add mobx for state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 
-import { API } from 'api/API'
+import { useStore } from 'store/root.store'
 
 import { Actors } from 'components/Actors/Actors'
 import { ActorAdd } from 'components/ActorAdd/ActorAdd'
@@ -8,73 +8,14 @@ import { Footer } from 'components/Footer/Footer'
 import { Header } from 'components/Header/Header'
 import { SortActors } from 'components/SortActors/SortActors'
 
-function App() {
-  const [actors, setActors] = useState([])
+const App = () => {
+  const { actorStore } = useStore()
 
-  // UTILS
-  const getActorIndexById = (actors, actorToSearch) => {
-    const checkActorId = (actor) => actor.id === actorToSearch.id
-    const actorIndex = actors.findIndex(checkActorId)
+  const { fetchActors, handleActorSubmit } = actorStore
 
-    return actorIndex
-  }
-
-  const checkActorUpdatedSuccesfully = (actor) => actor?.id
-
-  const getActorsAfterUpdate = (actorUpdated) => {
-    const actorUpdatedIndex = getActorIndexById(actors, actorUpdated)
-    const actorsLocal = [...actors]
-
-    actorsLocal[actorUpdatedIndex] = actorUpdated
-
-    return actorsLocal
-  }
-
-  const getActorsAfterDelete = (actorUpdated) => {
-    const actorUpdatedIndex = getActorIndexById(actors, actorUpdated)
-    const actorsLocal = [...actors]
-
-    actorsLocal.splice(actorUpdatedIndex, 1)
-
-    return actorsLocal
-  }
-
-  // REQUESTS
-  const fetchActors = async () => {
-    const actors = await API.getActors()
-    setActors(actors)
-  }
-
-  const handleActorSubmit = async (actor) => {
-    const actorNew = await API.postActor(actor)
-
-    if (checkActorUpdatedSuccesfully(actorNew)) {
-      setActors([...actors, actorNew])
-    }
-  }
-
-  const handleActorUpdate = async (actor) => {
-    const actorUpdated = await API.updateActor(actor)
-
-    if (checkActorUpdatedSuccesfully(actorUpdated)) {
-      const actorsUpdated = getActorsAfterUpdate(actorUpdated)
-      setActors(actorsUpdated)
-    }
-  }
-
-  const handleActorDelete = async (actor) => {
-    const actorDeleted = await API.deleteActor(actor)
-
-    if (checkActorUpdatedSuccesfully(actorDeleted)) {
-      const actorsUpdated = getActorsAfterDelete(actorDeleted)
-      setActors(actorsUpdated)
-    }
-  }
-
-  // EFFECTS
   useEffect(() => {
     fetchActors()
-  }, [])
+  }, [fetchActors])
 
   return (
     <div className='app'>
@@ -83,11 +24,7 @@ function App() {
       <div className='app-container'>
         <SortActors />
 
-        <Actors
-          actors={actors}
-          onActorUpdate={handleActorUpdate}
-          onActorDelete={handleActorDelete}
-        />
+        <Actors />
 
         <ActorAdd onActorSubmit={handleActorSubmit} />
       </div>
